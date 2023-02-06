027e19b8f4fb46e6671115f5f75d87457cb01360
ksklaura <ksklaura@gmail.com>
2023-02-06 16:58:58 +0900

    02/06 17:00

--- main.py
+++ main.py
@@ -76,7 +76,45 @@ print(a[5])
 print(a[-1]) # 뒤에서 첫번째 문자를 나타냄.
 
 a = "20230331Rainy"
-date = a[:8]
-weather = a[8:]
+date = a[:8] # 8 미포함
+weather = a[8:] # 8 포함
 print(date)
 print(weather)
+year = a[:4] # 처음부터 3까지
+day = a[4:8] # 4부터 7까지
+weather = a[8:] #8부터 마지막까지
+
+print("I eat %d apples." %3) # %d = 문자열 포맷 코드
+print("I eat %s apples." %"five") # %s = 문자열 바로 대입
+
+number = 3
+print("I eat %d apples." %number)
+
+number = 10
+day = "three"
+print("I ate %d apples, so I was sick for %s days." %(number, day))
+
+a = "Python is best choice"
+print(a.find('k'))
+
+print(4 > 10)
+print(not True)
+
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 "+animal+"의 이름은 "+name+"입니다.")
+hobby = "공놀이"
+print(name+"는 "+str(age)+"살이며, "+hobby+"를 아주 좋아해요.")
+print(name, "는 어른일까요? ", is_adult)
+
+# QUIZ) 변수를 이용하여 다음 문장을 출력하시오.
+# 변수명 : station
+# 변수값 : "사당", "신도림", "인천공항" 순서대로 입력
+# 출력문장 : XX행 열차가 들어오고 있습니다.
+
+station = "사당"
+print(station+"행 열차가 들어오고 있습니다.")
