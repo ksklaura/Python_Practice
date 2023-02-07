bb95a23b3a030cf19930397e5ffe4b0eb0002085
ksklaura <ksklaura@gmail.com>
2023-02-07 10:51:48 +0900

    02/07 10:30

--- main.py
+++ main.py
@@ -162,3 +162,26 @@ print(randrange(1,46)) # 1~46 미만의 임의값 생성
 from random import *
 date = randint(4, 28) # 4~28 사이의 임의값 생성 (4와 28 포함)
 print("오프라인 스터디 모임 날짜는 매월 "+str(date)+"일로 선정되었습니다.")
+
+python = "Python is amazing!"
+print(python.replace("Python", "Java"))
+print(python.index("n"))
+
+age = 20
+color = "빨간"
+print(f"나는 {age}살이며, {color}색을 좋아해요.")
+
+# QUIZ) 사이트 별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+
+# 예) http://naver.com
+# 규칙 1 : http:// 부분은 제외 => naver.com
+# 규칙 2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+# 규칙 3 : 남은 글자 중 처음 세 자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
+# 예) 생성된 비밀번호 : nav51!
+
+url = "http://google.com"
+my_str = url.replace("http://", "")
+my_str = my_str[:my_str.index(".")]
+print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}의 비밀번호는 \"{1}\"입니다.".format(url, password))
