78bee316fd1ff2cb56e23b825a1bcd25e4671dda
ksklaura <ksklaura@gmail.com>
2023-02-16 17:47:58 +0900

    02/16 17:45

--- practice3.py
+++ practice3.py
@@ -24,3 +24,42 @@ balance = deposit(balance, 1000)
 balance = withdraw(balance, 2000)
 commission, balance = withdraw_night(balance, 500)
 print("수수료 {0}원이며, 잔액은 {1}원입니다.".format(commission, balance))
+
+# 지역변수와 전역변수
+
+# QUIZ) 표준 체중을 구하는 프로그램을 작성하시오
+# * 표준 체중 : 각 개인의 키에 적당한 체중
+# (성별에 따른 공식)
+# 남자 : 키(m) x 키(m) x 22
+# 여자 : 키(m) x 키(m) x 21
+
+# 조건 1 : 표준 체중은 별도의 함수 내에서 계산
+#         * 함수명: std_weight
+#         * 전달값: 키(height), 성별(gender)
+# 조건 2 : 표준 체중은 소수점 둘째 자리까지 표시
+
+# (출력 예제)
+# 키 175cm 남자의 표준 체중은 67.38kg 입니다.
+
+
+def std_weight(height, gender): # 키는 m단위 (실수)
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
+
+height = 175
+gender = "남자"
+weight = round(std_weight(height/100, gender), 2)
+print("키 {0}cm {1}의 표중 체중은 {2}kg 입니다.".format(height, gender, weight))
+
+# 표준 입출력
+
+print("Python", "Java", "JavaScript", sep=" vs ")
+print("Python", "Java", "JavaScript", sep=", ", end="?")
+print("")
+
+scores = {"수학":0, "영어":50, "코딩":100}
+for subject, score in scores.items():
+    print(subject.ljust(8), str(score).rjust(4), sep=":") # ljust = 왼쪽 정렬, rjust = 오른쪽 정렬
+
