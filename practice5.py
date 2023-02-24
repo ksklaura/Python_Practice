7c3ed7d783dcb83120d32444739626236911d43a
ksklaura <ksklaura@gmail.com>
2023-02-24 15:47:01 +0900

    02/24 15:45

---
+++ practice5.py
@@ -0,0 +1,11 @@
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+marine1 = Unit("Marine", 100, 10)
+marine2 = Unit("Marine", 80, 5)
+tank = Unit("Tank", 150, 35)
