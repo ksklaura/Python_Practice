efacf982936ac472cb93145a9dac097906aa4e45
ksklaura <ksklaura@gmail.com>
2023-02-24 14:17:43 +0900

    02/24 14:20

--- mod1.py
+++ mod1.py
@@ -8,6 +8,3 @@ if __name__ == "__main__":
     print(add(1, 4))
     print(sub(4, 2))
 
-# TODO 주석은 일반 주석과 색이 다르게 표시되지요
-# TODO 되게 신기하당
-# 이건 일반 주석
--- practice3.py
+++ practice3.py
@@ -63,3 +63,37 @@ scores = {"수학":0, "영어":50, "코딩":100}
 for subject, score in scores.items():
     print(subject.ljust(8), str(score).rjust(4), sep=":") # ljust = 왼쪽 정렬, rjust = 오른쪽 정렬
 
+for num in range(1, 21):
+    print("대기번호 : "+str(num).zfill(3)) # zfill = 빈칸을 0으로 채움
+
+answer = input("아무 값이나 입력하세요 : ")
+print("입력하신 값은 " + answer + "입니다.")
+
+answer1 = 10
+print(type(answer1))
+
+# 빈 자리는 빈 공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간 확보
+print("{0: >10}".format(500))
+
+# 양수일 때는 +로 표시, 음수일 때는 -로 표시
+print("{0: >+10}".format(500))
+print("{0: >+10}".format(-500))
+
+# 왼쪽 정렬하고, 빈칸을 _로 채움
+print("{0:_<10}".format(500))
+
+# 3자리마다 콤마를 찍어주기
+print("{0:,}".format(100000000000))
+
+# 3자리마다 콤마를 찍어주기, +- 부호도 붙이기
+print("{0:+,}".format(100000000000))
+print("{0:-,}".format(-100000000000))
+
+# 3자리마다 콤마를 찍어주기, +- 부호도 붙이기, 자릿수 확보하기
+# 돈이 많으면 행복하니까 빈 자리는 ^로 채워주기
+print("{0:^<+30,}".format(100000000000))
+
+# 소수점 출력
+print("{0:f}".format(5/3))
+# 소수점 특정 자리수 까지만 표시 (소수점 3째 자리에서 반올림)
+print("{0:.2f}".format(5/3))
