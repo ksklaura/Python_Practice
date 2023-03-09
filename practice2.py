85021fcee09b865644ee219e545853059abb1c39
ksklaura <ksklaura@gmail.com>
2023-03-09 14:40:56 +0900

    03/09 14:40

--- practice2.py
+++ practice2.py
@@ -71,7 +71,7 @@ from random import *
 cnt = 0 # 총 탑승 승객 수
 for i in range(1, 51): # 1 ~ 50 사이의 수 (승객)
     time = randrange(5, 51) # 5분 ~ 50분 소요 시간
-    if 5 <= time <= 15: # 5분 ~ 15분 이내로 소요되는 손님, 탑승 승객 수 증가 처리
+    if 5 <= time <= 15: # 5분 ~ 15분 이내로 소요 되는 손님, 탑승 승객 수 증가 처리
         print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
         cnt += 1
     else: # 매칭 실패한 경우
